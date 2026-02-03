e7bbb0447f764bcb276abf9f9955fd353f5b9d30
Moharps <moharps8011@gmail.com>
2026-02-03 15:49:12 +0300

    Order form with JavaScript handling

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.getElementById("orderBtn").addEventListener("click", function () {
   window.open("https://wa.me/254722453814", "_blank");
 });
+
+
+
+document.getElementById("orderForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const order = document.getElementById("order").value;
+
+  document.getElementById("message").textContent =
+    `Thanks ${name}! Your order for "${order}" has been received.`;
+});
